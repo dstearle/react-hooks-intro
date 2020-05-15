9df967d2071532bcab3a7e65d23559540bc6f217
dstearle <dallas.st.earle@gmail.com>
2020-05-14 17:09:55 -0700

    Initial set up for App.js file and created a todo list in the state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	// State
+	const [todos, setTodos] = useState([
+
+		{
+			text: 'Learn about React',
+			isCompleted: false
+		},
+
+		{
+			text: 'Learn about Angular',
+			isCompleted: false
+		},
+
+		{
+			text: 'Learn about Vue',
+			isCompleted: false
+		},
+
+	]);
+
+	// Markup
+	return (
+
+		<div className="app">
+
+
+		</div>
+
+	)
+
 }
 
 export default App;
